7c698025bcfd90f32bbe72bf3ef98bc47896d108
aizr <aizr1212@gmail.com>
2013-06-06 14:56:22 +0800

    boards_controller.rb remove action new , create, edit, update, destroy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -16,40 +16,4 @@ class BoardsController < ApplicationController
     end
   end
   
-  def new
-    @board = Board.new
-  end
-    
-  def edit
-    @board = Board.find(params[:id])
-  end
-  
-  def create
-    @board = Board.new(params[:board])
-    
-    if @board.save
-      respond_to do |format|
-      format.html { redirect_to :action => :index }
-      end
-    end
-  end
-  
-  def update
-    @board = Board.find(params[:id])
-    
-    respond_to do |format|
-      if @board.update_attributes(params[:post])
-        format.html { redirect_to(@boards) }
-      end
-    end
-  end
-  
-  def destroy
-    @board = Board.find(params[:id])
-    @board.destroy
-    
-    respond_to do |format|
-      format.html { redirect_to(boards_url) }
-      end
-    end
   end
