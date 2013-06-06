7e195a1730ad25102bc5126682b3f50e52f2b60d
aizr <aizr1212@gmail.com>
2013-06-06 15:55:44 +0800

    modify app/controllers/admin/posts_controller.rb

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,35 +2,9 @@ class Admin::PostsController < ApplicationController
   layout 'admin'
   before_filter :require_is_admin
   before_filter :find_board
-  before_filter :authenticate_user!, :except => [:show, :index]
-  def index
-    redirect_to board_path(@board)
-  end
-  
-  def show
-    @post = @board.posts.find(params[:id])
-  end
-  
-  def new
-    @post = @board.posts.build      
-  end
-  
-  def edit
-    @post = current_user.posts.find(params[:id])
-  end
-  
-  def create
-    @post.user_id = current_user.id
-    
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to board_posts_path(@board), :notice => 'Post was successfully updated.' }
-      end
-    end
-  end
   
   def update
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
     
     respond_to do |format|
       if @post.update_attributes(params[:post])
@@ -40,7 +14,7 @@ class Admin::PostsController < ApplicationController
   end
   
   def destroy
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
     @post.destroy
     
     respond_to do |format|
