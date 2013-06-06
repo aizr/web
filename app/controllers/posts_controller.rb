835dfae0e42f42780f56d1de9bc07f8906870c0a
aizr <aizr1212@gmail.com>
2013-06-06 11:36:34 +0800

    add before_filter function and modify indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,55 +1,53 @@
 class PostsController < ApplicationController
-  
+  before_filter :find_board
   def index
-    @board = Board.find(params[:board_id])
     redirect_to board_path(@board)
   end
   
   def show
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
   end
   
   def new
-    @board = Board.find(params[:board_id])
     @post = @board.posts.build      
   end
   
   def edit
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
   end
   
   def create
-    @board = Board.find(params[:board_id])
     @post = @board.posts.build(params[:post])
     
     respond_to do |format|
       if @post.save
-      format.html { redirect_to board_posts_path(@board), :notice => 'Post was successfully updated.' }
+        format.html { redirect_to board_posts_path(@board), :notice => 'Post was successfully updated.' }
+      end
     end
-  end
   end
   
   def update
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
     
     respond_to do |format|
       if @post.update_attributes(params[:post])
-      format.html { redirect_to board_post_path(@board,@post), :notice => 'Post was successfully updated.' }
+        format.html { redirect_to board_post_path(@board,@post), :notice => 'Post was successfully updated.' }
+      end
     end
-  end
   end
   
   def destroy
-    @board = Board.find(params[:board_id])
     @post = @board.posts.find(params[:id])
     @post.destroy
     
     respond_to do |format|
       format.html { redirect_to board_posts_path(@board,@post) }
     end
+  end
+  
+  prorected
+  def find_board
+    @board = Board.find(params[:board_id])
   end
        
 end
