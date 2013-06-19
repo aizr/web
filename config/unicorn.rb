e2e151dbf7971f66c1cf191da201b619afeea92b
aizr <aizr1212@gmail.com>
2013-06-19 14:58:29 +0800

    modify unicorm string

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,8 @@ worker_processes 1
 listen "/home/apps/web/shared/sockets/unicorn.sock"
 
 project_dir = "/home/web/current"
+working_directory project_dir
 
-stderr_path "#{porject_dir}/log/unicorn_error.log"
+stderr_path "#{project_dir}/log/unicorn_error.log"
 stdout_path "#{project_dir}/log/unicorn.log"
 pid "#{project_dir}/tmp/pids/unicorn.pid"
