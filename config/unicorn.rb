f531b8278734a9426716aaeee67be66c53dae3e3
aizr <aizr1212@gmail.com>
2013-06-20 12:13:32 +0800

    enable preload_app

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,3 +8,15 @@ working_directory project_dir
 stderr_path "#{project_dir}/log/unicorn_error.log"
 stdout_path "#{project_dir}/log/unicorn.log"
 pid "#{project_dir}/tmp/pids/unicorn.pid"
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
